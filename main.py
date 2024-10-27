b21d0af840bc524d32a3eeaadaaa2a1b0d52e08c
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2024-10-27 15:36:36 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+# TODO make a 2 starter code examples for students
+
+# api ideas
+# https://github.com/public-apis/public-apis
+# https://unsplash.com/developers
+# https://developer.ticketmaster.com/products-and-docs/apis/getting-started/
+# https://www.deckofcardsapi.com
+# https://openweathermap.org/api
+# https://developer.kroger.com/api-products
