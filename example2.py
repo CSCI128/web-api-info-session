59c1aff324b0e4a021a21b8bea8262412d5dd98c
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2024-11-02 19:56:40 -0600

    add ticketmaster example

--- example2.py
+++ example2.py
@@ -1,3 +1,29 @@
 # TICKETMASTER API EXAMPLE
 # https://developer.ticketmaster.com/products-and-docs/apis/getting-started/
 import requests
+
+API_KEY = 'O25rm2nh73Airm6ZVRGIWW4HEsLGSKAP'
+
+# Event Search
+# https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/#search-events-v2
+resp = requests.get(f'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&postalCode=80401&apikey={API_KEY}')
+resp = resp.json()
+
+# get all events in golden
+for e in resp['_embedded']['events']:
+    print(e['id'])
+    print(e['name'])
+    print(e['url'])
+    print(e['dates'])
+    print(e['sales'])
+
+print("\n\n") # separator for output
+
+# Event Details
+# https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/#event-details-v2
+id = 'Z7r9jZ1A7C3AV'
+resp = requests.get(f'https://app.ticketmaster.com/discovery/v2/events/{id}.json?apikey={API_KEY}')
+resp = resp.json()
+
+print(resp['name'])
+print(resp['url'])
